c81a1062711174cc7cced3a3a68a9156bd8dd8fc
begummakin <86551606+begummakin@users.noreply.github.com>
2022-09-13 10:59:02 +0300

    Add files via upload

---
+++ Quiz/Quiz/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿using System;
+
+namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label lblQuestion;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnA = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.lblFalse = new System.Windows.Forms.Label();
+            this.lblQValue = new System.Windows.Forms.Label();
+            this.lblTValue = new System.Windows.Forms.Label();
+            this.lblFValue = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnrestart = new System.Windows.Forms.Button();
+            lblQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            lblQuestion.AutoSize = true;
+            lblQuestion.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            lblQuestion.Location = new System.Drawing.Point(483, 35);
+            lblQuestion.Name = "lblQuestion";
+            lblQuestion.Size = new System.Drawing.Size(97, 23);
+            lblQuestion.TabIndex = 0;
+            lblQuestion.Text = "Soru No:";
+            // 
+            // btnA
+            // 
+            this.btnA.Enabled = false;
+            this.btnA.Location = new System.Drawing.Point(14, 156);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(211, 32);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.Location = new System.Drawing.Point(14, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(428, 128);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "Welcome, Please Press Start Button.";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // btnB
+            // 
+            this.btnB.Enabled = false;
+            this.btnB.Location = new System.Drawing.Point(231, 156);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(211, 32);
+            this.btnB.TabIndex = 4;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Enabled = false;
+            this.btnC.Location = new System.Drawing.Point(14, 194);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(211, 32);
+            this.btnC.TabIndex = 5;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Enabled = false;
+            this.btnD.Location = new System.Drawing.Point(231, 194);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(211, 32);
+            this.btnD.TabIndex = 6;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTrue.Location = new System.Drawing.Point(502, 72);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(78, 23);
+            this.lblTrue.TabIndex = 7;
+            this.lblTrue.Text = "Doğru:";
+            // 
+            // lblFalse
+            // 
+            this.lblFalse.AutoSize = true;
+            this.lblFalse.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFalse.Location = new System.Drawing.Point(500, 109);
+            this.lblFalse.Name = "lblFalse";
+            this.lblFalse.Size = new System.Drawing.Size(75, 23);
+            this.lblFalse.TabIndex = 8;
+            this.lblFalse.Text = "Yanlış:";
+            // 
+            // lblQValue
+            // 
+            this.lblQValue.AutoSize = true;
+            this.lblQValue.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblQValue.Location = new System.Drawing.Point(584, 37);
+            this.lblQValue.Name = "lblQValue";
+            this.lblQValue.Size = new System.Drawing.Size(22, 23);
+            this.lblQValue.TabIndex = 9;
+            this.lblQValue.Text = "0";
+            // 
+            // lblTValue
+            // 
+            this.lblTValue.AutoSize = true;
+            this.lblTValue.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTValue.Location = new System.Drawing.Point(584, 73);
+            this.lblTValue.Name = "lblTValue";
+            this.lblTValue.Size = new System.Drawing.Size(22, 23);
+            this.lblTValue.TabIndex = 10;
+            this.lblTValue.Text = "0";
+            // 
+            // lblFValue
+            // 
+            this.lblFValue.AutoSize = true;
+            this.lblFValue.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFValue.Location = new System.Drawing.Point(584, 109);
+            this.lblFValue.Name = "lblFValue";
+            this.lblFValue.Size = new System.Drawing.Size(22, 23);
+            this.lblFValue.TabIndex = 11;
+            this.lblFValue.Text = "0";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(469, 141);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(167, 32);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = "Sonraki";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(471, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(561, 176);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(372, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(469, 9);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 17;
+            this.btnstart.Text = "Start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnrestart
+            // 
+            this.btnrestart.Location = new System.Drawing.Point(561, 9);
+            this.btnrestart.Name = "btnrestart";
+            this.btnrestart.Size = new System.Drawing.Size(75, 23);
+            this.btnrestart.TabIndex = 18;
+            this.btnrestart.Text = "Restart";
+            this.btnrestart.UseVisualStyleBackColor = true;
+            this.btnrestart.Visible = false;
+            this.btnrestart.Click += new System.EventHandler(this.btnrestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 256);
+            this.Controls.Add(this.btnrestart);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblFValue);
+            this.Controls.Add(this.lblTValue);
+            this.Controls.Add(this.lblQValue);
+            this.Controls.Add(this.lblFalse);
+            this.Controls.Add(this.lblTrue);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(lblQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label lblFalse;
+        private System.Windows.Forms.Label lblQValue;
+        private System.Windows.Forms.Label lblTValue;
+        private System.Windows.Forms.Label lblFValue;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private EventHandler button5_Click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnrestart;
+    }
+}
+
---
+++ Quiz/Quiz/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int questionNo = 0, True = 0, False = 0, score = 0;
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            label2.Text = btnB.Text;
+            if (label2.Text == label1.Text)
+            {
+                True++;
+                lblTValue.Text = True.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                False++;
+                lblFValue.Text = False.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            label2.Text = btnC.Text;
+            if (label2.Text == label1.Text)
+            {
+                True++;
+                lblTValue.Text = True.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                False++;
+                lblFValue.Text = False.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            questionNo++;
+            lblQValue.Text = questionNo.ToString();
+            btnA.Enabled = true;
+            btnB.Enabled = true;
+            btnC.Enabled = true;
+            btnD.Enabled = true;
+            if (questionNo > 0)
+            {
+                btnstart.Visible = false;
+            }
+            if (questionNo == 1)
+            {
+                richTextBox1.Text = "When was elon musk born?";
+                btnA.Text = "1971";
+                btnB.Text = "1970";
+                btnC.Text = "1985";
+                btnD.Text = "1965";
+                label1.Text = "1971";
+                btnrestart.Visible = true;
+            }
+        }
+
+        private void btnrestart_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            questionNo = 0;
+            True = 0;
+            False = 0;
+            lblQValue.Text = questionNo.ToString();
+            lblTValue.Text = True.ToString();
+            lblFValue.Text = False.ToString();
+            richTextBox1.Text = "Welcome, Please press Start Button.";
+            btnstart.Visible = true;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            label2.Text = btnD.Text;
+            if (label2.Text == label1.Text)
+            {
+                True++;
+                lblTValue.Text = True.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                False++;
+                lblFValue.Text = False.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            btnA.Enabled = false;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnNext.Enabled = true;
+
+            label2.Text = btnA.Text;
+            if (label2.Text == label1.Text)
+            {
+                True++;
+                lblTValue.Text = True.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                False++;
+                lblFValue.Text = False.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            btnA.Enabled = true;
+            btnB.Enabled = true;
+            btnC.Enabled = true;
+            btnD.Enabled = true;
+            btnNext.Enabled = false;
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            questionNo++;
+            lblQValue.Text = questionNo.ToString();
+
+            if (questionNo == 1)
+            {
+                richTextBox1.Text = "When was elon musk born?";
+                btnA.Text = "1971";
+                btnB.Text = "1970";
+                btnC.Text = "1985";
+                btnD.Text = "1965";
+                label1.Text = "1971";
+            }
+            if (questionNo == 2)
+            {
+                richTextBox1.Text = "What is the tallest mountain in the world?";
+                btnA.Text = "Himalaya";
+                btnB.Text = "Toroslar";
+                btnC.Text = "Everest";
+                btnD.Text = "Fuji";
+                label1.Text = "Everest";
+            }
+            if (questionNo == 3)
+            {
+                richTextBox1.Text = "Who is the president of ukraine?";
+                btnA.Text = "Elon Musk";
+                btnB.Text = "Putin";
+                btnC.Text = "Volodimir Zelenski";
+                btnD.Text = "Boris Johnson";
+                label1.Text = "Volodimir Zelenski";
+            }
+            if (questionNo == 4)
+            {
+                richTextBox1.Text = "When was the second world war?";
+                btnA.Text = "1939-1945";
+                btnB.Text = "1920-1935";
+                btnC.Text = "1945-1956";
+                btnD.Text = "1925-1940";
+                label1.Text = "1939-1945";
+            }
+            if (questionNo == 5)
+            {
+                richTextBox1.Text = "What state of matter is the sun?";
+                btnA.Text = "Gas";
+                btnB.Text = "Fıre";
+                btnC.Text = "Solid";
+                btnD.Text = "Plasma";
+                label1.Text = "Plasma";
+                btnNext.Text = "Results";
+
+            }
+            if (questionNo == 6)
+            {
+                btnA.Enabled = false;
+                btnB.Enabled = false;
+                btnC.Enabled = false;
+                btnD.Enabled = false;
+                btnNext.Enabled = false;
+                score = True * 4 - False * 1;
+                MessageBox.Show("True: " + True + "\n" + "False: " + False + "\n" + "Score: " + score);
+            }
+
+        }
+    }
+}
